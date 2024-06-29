49f035d5b24aaf71979ee1c0d38f86733f08e143
spencerrenfro <spencer.renfro13@gmail.com>
2024-06-28 22:54:14 -0600

    modals to portal version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-//pages
+
+// Pages
 import AddItem from "./pages/AddItem";
 import Checkout from "./pages/Checkout";
 import Inventory from "./pages/Inventory";
@@ -11,7 +11,8 @@ import SingleItemInfo from "./pages/ItemInfo";
 import SingleBarcode from "./pages/SingleBarcode";
 import ItemCreationFailure from "./pages/ItemCreationFailure";
 import ItemCreationSuccessful from "./pages/ItemCreationSuccessful";
-//components
+
+// Components
 import Navbar from "./ui/Navbar";
 import Modal from "./ui/modal/Modal";
 import FilterModal from "./ui/modal/FilterModal";
@@ -19,12 +20,30 @@ import StatisticsModal from "./ui/modal/StatisticsModal";
 
 function App() {
   const [showModal, setShowModal] = useState(false);
-  const [showFilterModal, setShowFilterModal] = useState(true);
   const [showStatisticsModal, setShowStatisticsModal] = useState(false);
+  const [showFilterModal, setShowFilterModal] = useState(false);
   const [filter, setFilter] = useState("");
-  const modalHandler = () => {
-    setShowModal(!showModal);
+
+  const modalHandler = (filter, statistics) => {
+    setShowModal(true); // Always show the main Modal container
+
+    if (filter) {
+      setShowFilterModal(true); // Show the filter modal
+    } else {
+      setShowFilterModal(false); // Hide the filter modal
+    }
+
+    if (statistics) {
+      setShowStatisticsModal(true); // Show the statistics modal
+    } else {
+      setShowStatisticsModal(false); // Hide the statistics modal
+    }
   };
+
+  useEffect(() => {
+    console.log("filter changed");
+    console.log("filter: ", filter);
+  }, [filter]);
 
   return (
     <div className="dark:bg-slate-900 min-h-screen ">
@@ -44,8 +63,8 @@ function App() {
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/check-in" element={<CheckIn />} />
         <Route path="/logs" element={<Logs />} />
-        <Route path="inventory/:id" element={<SingleItemInfo />} />
-        <Route path="inventory/:barcode" element={<SingleBarcode />} />
+        <Route path="/inventory/:id" element={<SingleItemInfo />} />
+        <Route path="/inventory/:barcode" element={<SingleBarcode />} />
         <Route
           path="/item-creation-successful"
           element={<ItemCreationSuccessful />}
@@ -68,12 +87,10 @@ function App() {
             />
           )}
           {showStatisticsModal && (
-            <StatisticsModal
-              onClose={() => {
-                setShowStatisticsModal(false);
-                setShowModal(false);
-              }}
-            />
+            <StatisticsModal onClose={() => {
+              setShowStatisticsModal(false)
+              setShowModal(false)
+            }} />
           )}
         </Modal>
       )}
